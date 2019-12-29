fcc7f14711ad797333214450a64592ab02252e1b
rookie-zheng <944699017@qq.com>
2019-12-29 15:07:35 +0800

    添加打印功能，导出财务记录excel表

--- src/main/java/com/rookie/controller/RecordController.java
+++ src/main/java/com/rookie/controller/RecordController.java
@@ -1,36 +1,37 @@
 package com.rookie.controller;
 
 
-import com.rookie.po.Output;
 import com.rookie.po.Record;
 import com.rookie.service.InputService;
 import com.rookie.service.OutputService;
 import com.rookie.service.RecordService;
+import com.rookie.util.ExcelUtils;
 import com.rookie.util.RemainingSum;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import javassist.NotFoundException;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import javax.validation.Valid;
+import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("/admin")
 @Api(tags = "后台登录接口")
 public class RecordController {
-
     @Autowired
     private RemainingSum remainingSum;
 
@@ -66,4 +67,34 @@ public class RecordController {
         return "admin/archives";
     }
 
+    @ApiOperation("打印财务记录")
+    @RequestMapping("/record/excel")
+    public String recordExcel(HttpServletResponse response){
+        List<Record> list = recordService.listRecord();
+        List<String> head = new ArrayList<>();
+        head.add("编号");
+        head.add("存入金额");
+        head.add("取出金额");
+        head.add("存入金钱时间");
+        head.add("取出金钱时间");
+        head.add("剩余金额");
+
+        List<List<String>> body = new ArrayList<>();
+        int i = 1;
+        for (Record re : list){
+            List<String> bodyValue = new ArrayList<>();
+            bodyValue.add(String.valueOf(i));
+            bodyValue.add(String.valueOf(re.getInputSum()));
+            bodyValue.add(String.valueOf(re.getOutputSum()));
+            bodyValue.add(String.valueOf(re.getInputTime()));
+            bodyValue.add(String.valueOf(re.getOutputTime()));
+            bodyValue.add(String.valueOf(re.getSum()));
+            body.add(bodyValue);
+            i++;
+        }
+        String fileName = "财务记录.xls";
+        HSSFWorkbook excel = ExcelUtils.expExcel(head,body);
+        ExcelUtils.outFile(excel,"./"+fileName,response);
+        return "redirect:/admin/archives";
+    }
 }
